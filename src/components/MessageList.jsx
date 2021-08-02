10c9acdbb0ac80a0e651b7e345009dc80a5dd757
otis wilcox <otiswilcox@gmail.com>
2021-08-02 14:03:40 -0400

    pushing up our test driven development in react work

---
+++ src/components/MessageList.jsx
@@ -0,0 +1,11 @@
+const MessageList = ({ messages }) => {
+  return (
+    <ul>
+      {messages.map((message, index) => (
+        <li key={`${index}-${message}`}>{message}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default MessageList;
